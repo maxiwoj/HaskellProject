d71d02239a35f6503002a6c7356ebb6fa96fabf8
maxiwoj <maxwojczuk@gmail.com>
2017-01-18 17:57:21 +0100

    added HUnitTests

--- test/Spec.hs
+++ test/Spec.hs
@@ -2,12 +2,17 @@ import Test.QuickCheck
 import Quicksort
 import MergeSort
 import Data.List
+import Test.HUnit
+import KMP
 
 -- main :: IO ()
 main = do
   quickCheck prop_merge
   quickCheck prop_mergeSort
   quickCheck prop_partFromQS
+  runTestTT testMerge
+  runTestTT testKMP
+
 
 -- prop_merge :: [Int] -> [Int] -> Bool
 prop_merge n m = (merge (sort n) (sort m)) == (sort (n ++ m))
@@ -22,3 +27,9 @@ prop_mergeSort n = (mergeSort n) == (sort n)
 -- przez to ze wrzucasz na poczatek listy w tej funkcji, to trzeba reversowec w testach
 prop_partFromQS xs y = (part xs y) == (reverse (filter (> y) xs), reverse (filter (== y) xs), reverse (filter (< y) xs))
   where types = (xs :: [Int], y :: Int)
+
+testKMP :: Test
+testKMP = TestCase $ assertEqual "Should give list of indexes where string test is" [16] (findPattern "test" "icuasdjvnacvadfitesttetetetetetstststetrteteset")
+
+testMerge :: Test
+testMerge = TestCase $ assertEqual "should give a sorted list" [1..1000] (mergeSort [1..1000])
