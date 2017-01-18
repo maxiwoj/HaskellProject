e9e869c77362c0697cbe2d71bdaf625b3ad78bb1
Chimstaz <tomasz.michalec1996@gmail.com>
2017-01-18 14:21:07 +0100

    Added reading file in haskel lib.hs

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,8 +9,15 @@ import MergeSort
 import Regression
 
 someFunc :: IO ()
-someFunc =  defaultMain [
-  bench "quicksort 1000" $ nf quicksort [(1::Int)..1000],
-  bench "mergeSort 1000" $ nf mergeSort [(1::Int)..1000],
-  bench "Linear Regression 10000 steps" $ nf (regress [(1::Double)..10]) [(11::Double)..20]
-  ]
+someFunc =  do
+  contentNumber10k <- readFile "Csrc/data/NumbersToSort10k.txt"
+  let numbersToSort  = map readInt . words $ contentNumber10k
+  defaultMain [
+      bench "quicksort 10000" $ nf quicksort numbersToSort,
+      bench "mergeSort 10000" $ nf mergeSort numbersToSort,
+      bench "Linear Regression 10000 steps" $ nf (regress [(1::Double)..10]) [(11::Double)..20]
+    ]
+
+
+readInt :: String -> Int
+readInt = read
