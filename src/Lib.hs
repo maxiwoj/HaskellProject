fbd7a5ec9895ccd822a03bc151efbcdce1e77423
Chimstaz <tomasz.michalec1996@gmail.com>
2017-01-19 20:47:26 +0100

    Added benching for regression in C and Haskell Changed test set for regression to contain only 100 lines. Deleted unused includes from C and C++ sources files.

--- src/Lib.hs
+++ src/Lib.hs
@@ -12,19 +12,31 @@ import KMP
 someFunc :: IO ()
 someFunc =  do
   contentNumber10k <- readFile "Csrc/data/NumbersToSort10k.txt"
-  let numbersToSort  = map readInt . words $ contentNumber10k
+  let numbersToSort = map readInt . words $ contentNumber10k
+
   contetKMP <- readFile "Csrc/data/KMP.txt"
   let kmplines = lines contetKMP
   let kmppattern = kmplines !! 0
   let kmpdata = kmplines !! 1
 
+  regressionData <- readFile "Csrc/data/testSetForRegression.txt"
+  let regDataXY = makeXY . (map readDouble) . words $ regressionData 
+
   defaultMain [
       bench "quicksort 10000" $ nf quicksort numbersToSort,
       bench "mergeSort 10000" $ nf mergeSort numbersToSort,
       bench "kmp 1000000" $ nf (findPattern kmppattern) kmpdata,
-      bench "Linear Regression 10000 steps" $ nf (regress [(1::Double)..10]) [(11::Double)..20]
+      bench "Linear Regression 10000 steps" $ nf (regress $ fst regDataXY) $ snd regDataXY
     ]
 
+makeXY :: [Double] -> ([Double], [Double])
+makeXY xs = makeXY' xs [] []
+  where
+    makeXY' [] xs ys = (xs, ys)
+    makeXY' (x:y:rest) xs ys = makeXY' rest (xs++[x]) (ys++[y])
+
+readDouble :: String -> Double
+readDouble = read
 
 readInt :: String -> Int
 readInt = read
