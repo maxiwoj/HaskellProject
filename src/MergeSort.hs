475d5608204e917a328b55f1f61038d2414354e8
maxiwoj <maxwojczuk@gmail.com>
2017-01-20 22:35:09 +0100

    Added comments into Regression and MergeSort

--- src/MergeSort.hs
+++ src/MergeSort.hs
@@ -4,13 +4,13 @@ module MergeSort
   ) where
 
 
---Funkcja przyjmujace 2 listy i złaczajaca je sortujac
+--Function takes 2 sorted lists and merges them into one sorted list
 merge :: Ord a => [a] -> [a] -> [a]
-merge xs [] = xs
+merge xs [] = xs --If one of the list is empty function returns the not empty list
 merge [] xs = xs
 merge (x:xs) (y:ys)
   | (x <= y)      = x : merge xs (y:ys)
-  | otherwise     = y : merge (x:xs) ys
+  | otherwise     = y : merge (x:xs) ys --the first element is the smaller one
 
 
 --Głowna funkcja odpowiadajaca za sortowanie Merge
@@ -18,5 +18,6 @@ mergeSort :: Ord a => [a] -> [a]
 mergeSort [] = []
 mergeSort [x] = [x]
 mergeSort xs = merge (mergeSort (fsthalf xs)) (mergeSort (sndhalf xs)) where
+  --checked multiple functions dividing list in half, but the fastest ones were:
   fsthalf = take (length xs `div` 2)
   sndhalf = drop (length xs `div` 2)
--- src/Regression.hs
+++ src/Regression.hs
@@ -1,4 +1,4 @@
--- regresja liniowa funkcji jednej zmiennej
+-- Linear Regression of one variable function
 module Regression
   ( regress,
     cost,
@@ -10,25 +10,38 @@ module Regression
 regress :: Fractional a => [a] -> [a] -> (a,a)
 regress x y = gradient_descent (0,0) x y 0.01 (length x) 10000
 
-
+{-
+returns distance between the function f(x) = theta_a * x + theta_b
+and the given point for given x
+-}
 cost1el :: Fractional a => (a,a) -> a -> a -> a
 cost1el (theta_a,theta_b) x_el y_el = (theta_a * x_el + theta_b - y_el) ^ 2
 
--- Obecny koszt prostej względem zbioru (suma kwadratow odległości kazdego z elementow od prostej)
+{-
+returns the sum of the cost of the function and a set of values
+it is a sum of squares of distance between the given function and all of the given elements
+-}
 cost :: Fractional a => (a,a) -> [a] -> [a] -> a
 cost theta x y = (1/(fromIntegral (length x))) * (foldl1 (+) (zipWith (cost1el theta) x y)) --suma kosztów dla każdego elementu pomnożona
 
-
+{-
+simple loop of taking a gradient step
+-}
 gradient_descent :: (Fractional a, Num b, Eq b, Integral b) => (a,a) -> [a] -> [a] -> a -> b -> b -> (a,a)
 gradient_descent (theta) _ _ _ _ 0 = (theta)
 gradient_descent theta x y alpha xLength steps =
   gradient_descent (gradient_step theta x y alpha xLength) x y alpha xLength (steps - 1)
 
--- ruch o 'alpha' w kierunku minimum funkcji kosztu (gradient)
+{-
+it is a step of a function into the minimum of the cost function (simple gradient)
+-}
 gradient_step :: (Fractional a, Num b, Eq b, Integral b) => (a,a) -> [a] -> [a] -> a -> b -> (a,a)
 gradient_step (theta_a, theta_b) x y alpha xLength = (theta_a - (alpha * delta_a), theta_b - (alpha * delta_b)) where
   delta_a = 2/((fromIntegral xLength)) * (foldl1 (+) (zipWith (\ x_el y_el -> (theta_a * x_el + theta_b - y_el) * x_el) x y))
   delta_b = 2/((fromIntegral xLength)) * (foldl1 (+) (zipWith (\ x_el y_el -> theta_a * x_el + theta_b - y_el) x y))
 
+{-
+returns a value of function (f(x) = theta_a * x + theta_b) for given x
+-}
 getY :: Fractional a => (a,a) -> a -> a
 getY (theta_a, theta_b) x = theta_a * x + theta_b
